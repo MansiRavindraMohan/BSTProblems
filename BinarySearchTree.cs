c9d68681e48c4d25ab4b9ab40e253ddd81429627
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-27 15:55:27 +0530

    Create BST by adding 56 and adding 30 and 70

---
+++ BinarySearchTree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTProblems
+{
+        public interface INode<T> where T : IComparable<T>
+        {
+            T Key { get; set; }
+            INode<T> Left { get; set; }
+            INode<T> Right { get; set; }
+        }
+
+        public class BinaryNode<T> : INode<T> where T : IComparable<T>
+        {
+            public T Key { get; set; }
+            public INode<T> Left { get; set; }
+            public INode<T> Right { get; set; }
+
+            public BinaryNode(T key)
+            {
+                Key = key;
+                Left = null;
+                Right = null;
+            }
+        }
+
+        public class BinarySearchTree<T> where T : IComparable<T>
+        {
+            public INode<T> Root { get; set; }
+
+            public BinarySearchTree(T key)
+            {
+                Root = new BinaryNode<T>(key);
+            }
+
+            public void AddNode(T key)
+            {
+                AddNode(Root, key);
+            }
+
+            private void AddNode(INode<T> node, T key)
+            {
+                if (key.CompareTo(node.Key) < 0)
+                {
+                    if (node.Left == null)
+                    {
+                        node.Left = new BinaryNode<T>(key);
+                    }
+                    else
+                    {
+                        AddNode(node.Left, key);
+                    }
+                }
+                else
+                {
+                    if (node.Right == null)
+                    {
+                        node.Right = new BinaryNode<T>(key);
+                    }
+                    else
+                    {
+                        AddNode(node.Right, key);
+                    }
+                }
+            }
+        }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree problems");
+
+            BinarySearchTree<int> bst = new BinarySearchTree<int>(56);
+            bst.AddNode(30);
+            bst.AddNode(70);
+
+            Console.WriteLine(bst.Root.Key);
+            Console.WriteLine(bst.Root.Left.Key);
+            Console.WriteLine(bst.Root.Right.Key);
         }
     }
 }
